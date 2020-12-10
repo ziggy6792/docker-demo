1dbd9b4d6436c9c59dd0b57bf1770e19ff362d11
Simon <simonaverhoeven@gmail.com>
2020-12-10 12:38:31 +0800

    added docker

--- lighthouserc.js
+++ lighthouserc.js
@@ -3,6 +3,9 @@ module.exports = {
     collect: {
       // staticDistDir: "./build",
       url: 'http://google.com/',
+      settings: {
+        'chrome-flags': '--no-sandbox --headless',
+      },
     },
     upload: {
       target: 'temporary-public-storage',
